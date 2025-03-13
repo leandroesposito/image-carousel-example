f9550cf7c2152b21f7ce55095db7e3af054c7a83
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-13 16:36:04 -0300

    Implement carousel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./style.css";
+
+const carousel = document.querySelector(".carousel");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const navButtons = document.querySelectorAll(".nav button");
+const carouselWidth = carousel.offsetWidth;
+const carouselContent = carousel.querySelector(".content");
+
+let carouselCurrentPageNumber = 0;
+
+function displayPage(numPage) {
+  const newPageIndex = Number(numPage);
+  if (newPageIndex < 0 || navButtons.length <= newPageIndex) {
+    return;
+  }
+
+  navButtons[carouselCurrentPageNumber].classList.toggle("current");
+  navButtons[newPageIndex].classList.toggle("current");
+  carouselContent.style.translate = `${-carouselWidth * newPageIndex}px`;
+  carouselCurrentPageNumber = newPageIndex;
+}
+
+navButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    displayPage(event.target.dataset.page);
+  });
+});
+
+leftArrow.addEventListener("click", () => {
+  displayPage(carouselCurrentPageNumber - 1);
+});
+
+rightArrow.addEventListener("click", () => {
+  displayPage(carouselCurrentPageNumber + 1);
+});
